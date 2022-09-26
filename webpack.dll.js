8afcd342fccd01f7ea4ba53eee7fb46033751a04
寒蝉 <549141745@qq.com>
2022-09-26 23:37:57 +0800

    添加DLL优化、BundleAnalyzer、SpeedMeasure

--- webpack.dll.js
+++ webpack.dll.js
@@ -6,20 +6,16 @@ const webpack = require('webpack')
 module.exports = {
   entry: {
     vendor: ['ant-design-vue']
-    // 将数组中的一些包打包为一个chunk，包名字就和键一样
-    // vue: ['vue','vuex','vue-router']
   },
   output: {
-    // [name] = entry的键名
     filename: '[name].js',
     path: resolve(__dirname, 'dll'),
-    library: '[name]_[fullhash]', //打包的dll文件作为新的包，暴露出去的变量名
+    library: '[name]_[fullhash]',
   },
   plugins: [
-    //生成一个目录，告诉webpack不要打包这些文件了
     new webpack.DllPlugin({
-      name: '[name]_[fullhash]', //映射库的名称
-      path: resolve(__dirname, 'dll/manifest.json') //生成一个库名和真正库的映射文件
+      name: '[name]_[fullhash]',
+      path: resolve(__dirname, 'dll/manifest.json') // 映射名称
     })
   ],
   mode: 'production'
