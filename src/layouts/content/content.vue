44a9db80025fb6281e9d5c9808ea3136f54e577d
zero_fsc <zero_fsc@outlook.com>
2022-09-26 17:04:12 +0800

    init

---
+++ src/layouts/content/content.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="content-container">content</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+  .content-container {
+    padding: 12px 24px;
+  }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { AntDesignVueResolver, Vue } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [AntDesignVueResolver()],
+        imports: ['vue'],
+      }),
+      Components({
+        extensions: ['vue'],
+        resolvers: [AntDesignVueResolver()],
+        dts: true,
+        deep: true
+      })
+    ]
+  }
+})
