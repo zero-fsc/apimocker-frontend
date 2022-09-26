ec8359e35418b62ccc88558a91917a969eec5053
寒蝉 <549141745@qq.com>
2022-09-27 01:45:09 +0800

    优化dll打包，

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
-const { AntDesignVueResolver, Vue } = require('unplugin-vue-components/resolvers')
+const {
+  AntDesignVueResolver,
+  Vue
+} = require('unplugin-vue-components/resolvers')
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const DllRefrencePlugin = require('webpack').DllReferencePlugin
-const { resolve } = require('path')
+const DllReferencePlugin = require('webpack').DllReferencePlugin
+const path = require('path')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
@@ -14,8 +21,29 @@ module.exports = defineConfig({
       new SpeedMeasurePlugin(),
       new BundleAnalyzerPlugin(),
       // DLL预打包
-      new DllRefrencePlugin({
-        manifest: resolve(__dirname, 'dll/manifest.json')
+      new DllReferencePlugin({
+        context: process.cwd(),
+        manifest: require(`./public/vendor/vue-manifest.json`)
+      }),
+      new DllReferencePlugin({
+        context: process.cwd(),
+        manifest: require(`./public/vendor/antd-manifest.json`)
+      }),
+      new AddAssetHtmlPlugin({
+        // dll文件位置
+        filepath: path.resolve(__dirname, './public/vendor/antd.dll.js'),
+        // dll 引用路径，请使用 绝对路径！！！
+        publicPath: '/vendor',
+        // dll最终输出的目录
+        outputPath: './vendor'
+      }),
+      new AddAssetHtmlPlugin({
+        // dll文件位置
+        filepath: path.resolve(__dirname, './public/vendor/vue.dll.js'),
+        // dll 引用路径，请使用 绝对路径！！！
+        publicPath: '/vendor',
+        // dll最终输出的目录
+        outputPath: './vendor'
       }),
       AutoImport({
         resolvers: [AntDesignVueResolver()],
--- webpack.dll.js
+++ webpack.dll.js
@@ -1,22 +1,31 @@
-const {
-  resolve
-} = require('path')
+const path = require('path')
 const webpack = require('webpack')
 
+// dll文件存放的目录
+const dllPath = 'public/vendor'
+
+
 module.exports = {
+  mode: 'development',
+  // 需要提取的库文件
   entry: {
-    vendor: ['ant-design-vue']
+    vue: ["vue", "vue-router", 'axios'],
+    antd: ["ant-design-vue"],
   },
   output: {
-    filename: '[name].js',
-    path: resolve(__dirname, 'dll'),
-    library: '[name]_[fullhash]',
+    path: path.join(__dirname, dllPath),
+    filename: '[name].dll.js',
+    // vendor.dll.js中暴露出的全局变量名
+    // 保持与 webpack.DllPlugin 中名称一致
+    library: '[name]_[hash]'
   },
   plugins: [
+    // 定义插件
     new webpack.DllPlugin({
-      name: '[name]_[fullhash]',
-      path: resolve(__dirname, 'dll/manifest.json') // 映射名称
+      path: path.join(__dirname, dllPath, '[name]-manifest.json'),
+      // 保持与 output.library 中名称一致
+      name: '[name]_[hash]',
+      context: process.cwd()
     })
-  ],
-  mode: 'production'
+  ]
 }
